0fb5dcb4eaa4b7476b6da8c67d1184b8558e7313
aweber1309 <123497208+aweber1309@users.noreply.github.com>
2023-11-30 19:01:40 -0700

    Add files via upload

--- routes/routes.js
+++ routes/routes.js
@@ -1,2 +1,18 @@
 const {response} = require('express')
 const mongoose = require('mongoose')
+
+const connectionString = 'mongodb://127.0.0.1:27017/'
+mongoose.connect(connectionString, {})
+
+const db = mongoose.connection
+
+db.on('error', console.error.bind(console, 'DB connection error'))
+
+db.once('open', callback => {})
+
+//views
+exports.index = (req, res) => {
+    res.render('index', {
+        title: 'title'
+    })
+}
