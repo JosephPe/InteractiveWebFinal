2d35c58f9bad9670a1c703b3d24afb1b4434fa58
aweber1309 <123497208+aweber1309@users.noreply.github.com>
2023-12-03 16:43:42 -0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,18 +1,22 @@
-const exp = require('constants')
-const express = require('express'),
-    pug = require('pug'),
-    path = require('path'),
-    routes = require('./routes/routes.js')
+const express = require('express');
+const path = require('path');
+const routes = require('./routes/routes.js');
+const bodyParser = require('body-parser');
 
-const app = express()
+const urlEncodedParser = bodyParser.urlencoded({ extended: true });
+const app = express();
 
-app.set('view engine', 'pug')
-app.set('views', path.join(__dirname, 'views'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', routes.index)
 app.get('/users', routes.users)
 app.get('/signup', routes.signup)
 app.get('/login', routes.login)
+app.post('/signup', urlEncodedParser, routes.postSignup)
+app.post('/login', urlEncodedParser, routes.postLogin)
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
--- public/login.js
+++ public/login.js
@@ -1,44 +1,45 @@
-const emailUsername = document.getElementById("emailUsername")
-const passwordInput = document.getElementById("password")
-const reply = document.getElementById("reply")
+const emailInput = document.getElementById("email");
+const passwordInput = document.getElementById("password");
+const reply = document.getElementById("reply");
 
-let validEmailUser = false
-let correctPassword = false
+let validEmail = false;
+let correctPassword = false;
 
-const validate = () => {
-    validateEmailUsernameInput()
-    validatePasswordInput()
+const validate = async () => {
+    await validateEmailInput();
+    await validatePasswordInput();
 
-    if(validEmailUser && correctPassword){
-        return true
-    }else{
-        reply.classList.remove('hide')
-        return false
+    if (validEmail && correctPassword) {
+        return true;
+    } else {
+        reply.classList.remove('hide');
+        return false;
     }
-}
+};
 
-const validateEmailUsernameInput = () => {
-    let emailUser = emailUsername.value
-    let databaseEmailUser = "test"
-
-    if(emailUser == databaseEmailUser){
-        validEmailUser = true
-        reply.classList.add('hide')
-        console.log("true")
-    }else{
-        console.log("false")
+const validateEmailInput = async () => {
+    let email = emailInput.value;
+    const response = await fetch('/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email }),
+    });
+    const data = await response.json();
+    if (data.success) {
+        validEmail = true;
+        reply.classList.add('hide');
+        console.log("true");
+    } else {
+        validEmail = false;
+        console.log("false");
     }
-}
+};
 
-const validatePasswordInput = () => {
-    let userPassword = passwordInput.value
-    let databasePassword = "test"
-
-    if(userPassword == databasePassword){
-        correctPassword = true
-        reply.classList.add('hide')
-        console.log("true")
-    }else{
-        console.log('false')
-    }
-}
+const validatePasswordInput = async () => {
+    let password = passwordInput.value;
+    correctPassword = true;
+    reply.classList.add('hide');
+    console.log("true");
+};
