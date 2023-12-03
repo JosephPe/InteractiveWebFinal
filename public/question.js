f59475fda3a4bbd595a5bf1deecfff879d27b1f6
HunterETate <htate@student.neumont.edu>
2023-12-03 12:01:50 -0700

    Create question.js

---
+++ public/question.js
@@ -0,0 +1,19 @@
+const quizContainer = document.getElementById('quiz-container');
+        const questions = [
+            {
+                question: 'What is the capital of France?',
+                options: ['Berlin', 'Paris', 'Rome', 'Madrid'],
+                correctAnswer: 'Paris'
+            },
+            {
+                question: 'What year did the Titanic sink?',
+                options: ['1905', '1912', '1920', '1931'],
+                correctAnswer: '1912'
+            },
+            {
+                question: 'What is the largest planet in our solar system?',
+                options: ['Venus', 'Mars', 'Jupiter', 'Saturn'],
+                correctAnswer: 'Jupiter'
+            }
+            // Add more questions if we need
+        ];
